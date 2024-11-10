e55704859e7c7f7b0b68c524fa0a9d8e87c4d656
breduarm <bryan.e.armijos@gmail.com>
2024-11-10 16:00:00 -0500

    feat(businessCard): Lay out the initial design

--- app/src/main/java/com/beam/businesscard/MainActivity.kt
+++ app/src/main/java/com/beam/businesscard/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.beam.businesscard.ui.theme.BusinessCardTheme
 
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BusinessCard(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +33,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        PersonalInfo()
+        ContactInfo()
+    }
+}
+
+@Composable
+fun PersonalInfo(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = null
+        )
+        Text(text = "Name")
+        Text(text = "Title")
+    }
+}
+
+@Composable
+fun ContactInfo(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        ContactInfoItem()
+        ContactInfoItem()
+        ContactInfoItem()
+    }
+}
+
+@Composable
+fun ContactInfoItem(modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null
+        )
+        Text(
+            text = "Text",
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }

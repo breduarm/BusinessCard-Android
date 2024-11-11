1328fa827cc69b412248bc3a45bfa8e3e36b5782
breduarm <bryan.e.armijos@gmail.com>
2024-11-11 11:00:00 -0500

    feat(businessCard): Add mocked info and styles

--- app/src/main/java/com/beam/businesscard/MainActivity.kt
+++ app/src/main/java/com/beam/businesscard/MainActivity.kt
@@ -5,17 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.beam.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,42 +42,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        PersonalInfo()
+    Column(
+        verticalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        PersonalInfo(fullName = "Bryan Armijos", title = "Android Developer")
         ContactInfo()
     }
 }
 
 @Composable
-fun PersonalInfo(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun PersonalInfo(fullName: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.android_logo),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier.height(100.dp)
         )
-        Text(text = "Name")
-        Text(text = "Title")
+        Text(
+            text = fullName,
+            fontSize = 40.sp,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
 fun ContactInfo(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        ContactInfoItem()
-        ContactInfoItem()
-        ContactInfoItem()
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ContactInfoItem(content = "+593 (98) 048 8810")
+        ContactInfoItem(content = "@breduarm")
+        ContactInfoItem(content = "bryan.e.armijos@gmail.com")
     }
 }
 
 @Composable
-fun ContactInfoItem(modifier: Modifier = Modifier) {
-    Row(modifier = modifier) {
+fun ContactInfoItem(content: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier.size(40.dp)
         )
         Text(
-            text = "Text",
+            text = content,
+            modifier = Modifier.padding(start = 8.dp)
         )
     }
 }

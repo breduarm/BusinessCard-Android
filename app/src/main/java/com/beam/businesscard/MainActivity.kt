b6727f35d6e3853d2393d58d201d6ef6dceff658
breduarm <bryan.e.armijos@gmail.com>
2024-11-12 11:00:00 -0500

    feat(businessCard): Add Spacers and adjust paddings

--- app/src/main/java/com/beam/businesscard/MainActivity.kt
+++ app/src/main/java/com/beam/businesscard/MainActivity.kt
@@ -5,20 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,12 +52,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BusinessCard(modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.SpaceAround,
+        verticalArrangement = Arrangement.SpaceEvenly,
         modifier = modifier
             .fillMaxSize()
             .padding(24.dp)
     ) {
+        Spacer(modifier = Modifier.weight(1f))
         PersonalInfo(fullName = "Bryan Armijos", title = "Android Developer")
+        Spacer(modifier = Modifier.weight(1f))
         ContactInfo()
     }
 }
@@ -63,7 +74,9 @@ fun PersonalInfo(fullName: String, title: String, modifier: Modifier = Modifier)
         Image(
             painter = painterResource(id = R.drawable.android_logo),
             contentDescription = null,
-            modifier = Modifier.height(100.dp)
+            modifier = Modifier
+                .height(100.dp)
+                .background(Color.Cyan)
         )
         Text(
             text = fullName,
@@ -80,31 +93,38 @@ fun PersonalInfo(fullName: String, title: String, modifier: Modifier = Modifier)
 @Composable
 fun ContactInfo(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ContactInfoItem(content = "+593 (98) 048 8810")
-        ContactInfoItem(content = "@breduarm")
-        ContactInfoItem(content = "bryan.e.armijos@gmail.com")
+        ContactInfoItem(content = "+593 (98) 048 8810", icon = Icons.Rounded.Phone)
+        ContactInfoItem(content = "@breduarm", icon = Icons.Rounded.Share)
+        ContactInfoItem(content = "bryan.e.armijos@gmail.com", icon = Icons.Rounded.Email)
     }
 }
 
 @Composable
-fun ContactInfoItem(content: String, modifier: Modifier = Modifier) {
+fun ContactInfoItem(content: String, icon: ImageVector, modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            imageVector = icon,
             contentDescription = null,
-            modifier = Modifier.size(40.dp)
+            modifier = Modifier.size(20.dp)
         )
         Text(
             text = content,
-            modifier = Modifier.padding(start = 8.dp)
+            fontSize = 14.sp,
+            modifier = Modifier
+                .width(200.dp)
+                .padding(start = 16.dp)
         )
     }
 }
